eb76ce7c8625ff9fbf54fd8d8ccc3f87aa6a37ed
François Moronval <francoismoronval@MacBook-Air-de-Francois.local>
2026-02-07 11:39:11 +0100

    Initial Quarto site

---
+++ Low_cost_airlines.R
@@ -0,0 +1,56 @@
+
+
+website:
+  title: "Digital Culture"
+navbar:
+  left:
+  - text: "Methodology"
+href: methodology.qmd
+- text: "Interpretation"
+href: interpretation.qmd
+- text: "Limitations"
+href: limitations.qmd
+- text: "Appendix"
+href: appendix.qmd
+- text: "Literature review"
+href: literaturereview.qmd
+- text: "Bibliography"
+href: bibliography.qmd
+
+format:
+  html:
+  
+  theme: cosmo
+toc: true
+
+
+
+
+
+
+
+
+
+
+project:
+  type: website
+
+website:
+  title: "Airline"
+navbar:
+  left:
+  - href: index.qmd
+text: Home
+- about.qmd
+- text: "Ryanair"
+href: methodology.qmd
+
+format:
+  html:
+  theme:
+  - cosmo
+- brand
+css: styles.css
+toc: true
+
+editor: visual
